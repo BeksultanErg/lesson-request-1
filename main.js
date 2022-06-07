4009687cab81d9b6a5ad21b3bff15317415e60c0
Beksultan <beksultan@MacBook-Pro-Beksultan.local>
2022-06-07 12:19:45 +0600

    added styles

--- main.js
+++ main.js
@@ -15,20 +15,43 @@ json-server -w db.json -p 8000
 CURD - Create(POST) Read(GET) Update(PUT/PATCH) Delete(DELETE)
 */
 
+
+//! Create
 const API = "http://localhost:8000/todo";
 
 const inpAdd = document.getElementById("inp-add");
 const btnAdd = document.getElementById("btn-add");
 
-btnAdd.addEventListener("click", function (){
+btnAdd.addEventListener("click", async function (){
    const newToDo = {
       todo: inpAdd.value,
    };
-   fetch(API, {
+   if(newToDo.todo.trim() === ""){
+      alert('Заполните поля!');
+      return;
+   }
+   await fetch(API, {
       method: "POST",
       body: JSON.stringify(newToDo),
       headers: {
          "Content-type": "application/json; charset=utf-8",
       },
    });
+   getTodos();
 });
+
+//! Read
+const list = document.getElementById('list');
+async function getTodos(params) {
+   const response = await fetch(API)
+   .then((res) => res.json())
+   .catch(err => console.log(err));
+   list.innerHTML = "";
+   console.log(response);
+   response.forEach((item) => {
+      const newElem = document.createElement('div');
+      newElem.innerHTML = `<span>${item.todo}</span>`;
+      list.append(newElem);
+   });
+}
+getTodos();
